ee15edad17b29db0250de3a0040f31abf7c1ff80
Ross Westwater <ross.westwater@gmail.com>
2021-11-01 12:45:40 -0600

    updated to loop through robots and reset health

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,8 +19,7 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+  while(enemyHealth >0) {
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -72,9 +71,12 @@ var fight = function(enemyName) {
     window.alert("You need to pick a valid option. Try again!");
   }
   //fight function statements
-};
+}
+}
 
 // run fight function to start game
-for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
-}
+for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
+} 
